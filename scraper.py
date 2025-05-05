f2ae6a3a1c2a26a9f3315f982334fa1ed55a8d5d
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-05-05 11:19:54 -0500

    added function declaration write out

--- scraper.py
+++ scraper.py
@@ -5,32 +5,65 @@
 # ]
 # ///
 import os
-from typing import Self
+from typing import List, Self
 import libcst as cst
+from libcst.display import dump
+
 
 class ProjectVisitor(cst.CSTVisitor):
-    def __init__(self: Self, output_file: str) -> None:
-        self.output_file = output_file
-        
+    def __init__(self: Self, module: cst.Module,  output_file: str) -> None:
+        self.output_file: str = output_file
+        self.module: cst.Module = module
+        self.separator: str = "-" * 50
         super().__init__()
 
-    
-    def visit_ClassDef(self, node) -> bool | None:
+    def visit_ClassDef(self: Self, node: cst.ClassDef) -> bool | None:
         class_name: str = node.name.value
 
         if class_name.startswith("_"):
             return
 
-        super_class_names = [base.value.value for base in node.bases]
-        super_class_names = [name if isinstance(name, str) else name.value for name in super_class_names]
+        super_class_names: List[str | cst.Name] = [
+            base.value.value for base in node.bases
+        ]
+        super_class_names: List[str] = [
+            name if isinstance(name, str) else name.value for name in super_class_names
+        ]
 
         with open(self.output_file, "a") as f:
-            inherit_string: str = f"inherit(s) from: {",".join(super_class_names)}" if len(super_class_names) >= 1 else "doesn't inherit from any class."
+            inherit_string: str = (
+                f"inherit(s) from: {','.join(super_class_names)}"
+                if len(super_class_names) >= 1
+                else "doesn't inherit from any class."
+            )
             f.write(
-                f"The name of the class: {class_name}, and this class {inherit_string}\n\n"
+                f"{self.separator}\n\nThe name of the class: {class_name}, and this class {inherit_string}\n\n"
             )
 
         return super().visit_ClassDef(node)
+
+    def leave_ClassDef(self: Self, original_node: cst.ClassDef) -> None:
+        with open(self.output_file, "a") as f:
+            f.write(f"{self.separator}\n\n")
+        
+        return super().leave_ClassDef(original_node)
+
+    def visit_FunctionDef(self: Self, node: cst.FunctionDef) -> bool | None:
+        function_name: str = node.name.value
+
+        if function_name.startswith("_") or function_name.startswith("__"):
+            return
+
+        function_definition = f"def {function_name}({self.module.code_for_node(node.params)})"
+        if node.returns is not None:
+            function_return = self.module.code_for_node(node.returns.annotation)
+            function_definition += f" -> {function_return}"
+        
+        with open(self.output_file, "a") as f:
+            f.write(f"{function_definition}\n")
+
+        return super().visit_FunctionDef_params(node)
+
 
 DOCS_FILE = "manim_docs.txt"
 if True:
@@ -42,8 +75,7 @@ for directory_path, _, file_names in os.walk("manimlib"):
         current_file: str = os.path.join(directory_path, file_name)
         if not current_file.endswith("py") or current_file.endswith("__init__.py"):
             continue
-
+        
         with open(current_file) as f:
-            code: cst.Module = cst.parse_module(f.read())
-            code.visit(ProjectVisitor(output_file=DOCS_FILE))
-            
+            code_module: cst.Module = cst.parse_module(f.read())
+            code_module.visit(ProjectVisitor(code_module, output_file=DOCS_FILE))
